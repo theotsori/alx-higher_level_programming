723bfb8bf3210d105996a6a07ca4baba10acb482
theotsori <theodoretsori@gmail.com>
2022-11-28 18:53:15 +0300

    Checked linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if there are loops
+ * in a linked list
+ * @list: the lnked list
+ * Return: always 0
+ */
 int check_cycle(listint_t *list)
 {
+	listint_t *firstP = list, *secondP = list;
+
+	while (secondP && firstP && firstP->next)
+	{
+		secondP = secondP->next;
+		firstP = firstP->next->next;
+		if (secondP == firstP)
+		{
+			return (1);
+		}
+	}
 	return (0);
 }
