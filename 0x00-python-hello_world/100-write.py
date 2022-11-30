09a3672f40f5c05b7b77689f019d8080ff873425
theotsori <theodoretsori@gmail.com>
2022-11-30 10:55:18 +0300

    write py update

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-
-def write(*a):
-
-    print(*a, file=sys.stderr)
-
-
-write("and that piece of art is useful - Dora Korpar, 2015-10-19")
-
-exit(1)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
