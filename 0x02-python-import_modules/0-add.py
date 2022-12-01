78be2184444392a9e44c768c6d0fc54a9b8157fb
theotsori <theodoretsori@gmail.com>
2022-12-01 16:22:29 +0300

    add py update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ from add_0 import add
 a = 1
 b = 2
 add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
