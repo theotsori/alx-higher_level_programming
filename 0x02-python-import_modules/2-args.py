255e6591be10143d1aa15a1d2b116a56ca3c7c00
theotsori <theodoretsori@gmail.com>
2022-12-01 18:46:04 +0300

    pyc files got me

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) <= 1:
-    print("{} argument".format(len(sys.argv)))
-else:
-    print("{}{}arguments".format(len(sys.argv), str(sys.argv)))
-#print(str(sys.argv))
-
-#if __name__ == "__main__":
-#    print(len(sys.argv[1]))
+n = len(sys.argv)
+for i in range(1, n):
+    if n == 1 in range(1, n):
+        print("0 arguments.")
+    if n == 2:
+        print("{} argument:\n{}: {}".format(n, n, sys.argv[i]))
+    else:
+        print("{}: {}".format(n, sys.argv[i]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-print("{}".format(alpha))
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for i in names:
+        if i[:2] != "__":
+            print(i)
