22d5c9ef8755adea4da0f6e55026d5101f7f7ec1
theotsori <theodoretsori@gmail.com>
2022-12-02 16:25:17 +0300

    py data structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     my_list[idx] = element
-    print(my_list)
+    return my_list
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,4 +2,4 @@
 def new_in_list(my_list, idx, element):
     i = my_list.index(idx+1)
     my_list = my_list[:i]+[element]+my_list[i+1:]
-    print(my_list)
+    return my_list
