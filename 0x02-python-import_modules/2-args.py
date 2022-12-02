dd6cfa21f8881088e9973ef0bbd839a38f98a1f8
theotsori <theodoretsori@gmail.com>
2022-12-02 15:10:39 +0300

    args update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
 n = len(sys.argv)
+
 for i in range(1, n):
     if n == 1 in range(1, n):
         print("0 arguments.")
     if n == 2:
         print("{} argument:\n{}: {}".format(n, n, sys.argv[i]))
     else:
-        print("{}: {}".format(n, sys.argv[i]))
+        k = len(sys.argv[i])
+        print("{}: {}".format(k, sys.argv[i]))
