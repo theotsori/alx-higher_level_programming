fbfc4939c741f92c78fddeb8eb83eeae06b19526
theotsori <theodoretsori@gmail.com>
2022-12-03 13:13:38 +0300

    print simple

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, 1)
-        return(c)
+        return (c)
     else:
-        return(sub(a, b))
+        return (sub(a, b))
