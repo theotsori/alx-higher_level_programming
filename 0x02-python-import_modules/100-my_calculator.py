d6323517d3dad74eb20638e358ca9ed713b1a58c
theotsori <theodoretsori@gmail.com>
2022-12-03 15:41:25 +0300

    100 calc doen

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
 
-
     if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> operator <b>")
         exit(1)
