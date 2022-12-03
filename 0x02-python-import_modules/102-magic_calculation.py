4d96f0a195ac711844b6f3bbba3a0e3fadf44e0c
theotsori <theodoretsori@gmail.com>
2022-12-03 11:35:13 +0300

    Alphabets in uppercase

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        sub
+
+    c = add(a, b)
+
+  
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(alphabet)
