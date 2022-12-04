47b519676193f176f86a088189b5f0205cb73512
theotsori <theodoretsori@gmail.com>
2022-12-04 13:39:04 +0300

    access secure py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > my_list[4]:
+        return None
+    else:
         return my_list[idx]
