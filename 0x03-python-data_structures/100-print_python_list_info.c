7aa8ef365830c35a7aa7b9bc4cd30f6688fabce1
theotsori <theodoretsori@gmail.com>
2022-12-05 12:17:11 +0300

    CPython is fun

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -8,22 +8,9 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	if (!PyList_Check(p))
-	{
-		fprintf(stderr, "Error: Input is not a Python list\n");
-		return;
-	}
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
 
-	int size = PyList_Size(p);
-
-	printf("[*] Size of the Python List = %d\n", size);
-
-	printf("[*] All elements of the Python List:\n");
-
-	for (int i = 0; i < size; i++)
-	{
-		PyObject *item = PyList_GetItem(p, i);
-
-		printf("Element %d: %s\n", i, PyUnicode_AsUTF8(PyObject_Repr(item)));
-	}
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocated);
 }
