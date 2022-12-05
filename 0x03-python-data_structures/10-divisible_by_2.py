3dd406dfb2ac9c1ef83d1b97f7731b4d52855175
theotsori <theodoretsori@gmail.com>
2022-12-05 08:01:25 +0300

    is divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
 
+    def is_multiple_2(x):
+        return x % 2 == 0
 
+    result = map(is_multiple_2, my_list)
+
+    return list(result)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == "":
         return None
-        
+
     mi = my_list[0]
 
     for x in my_list:
