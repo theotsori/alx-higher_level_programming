b074e9da729058750384b2319d204e70f399808e
theotsori <theodoretsori@gmail.com>
2022-12-05 12:30:18 +0300

    CPython is awesome

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,15 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_python_list_info - Function to print some basic
- * info about a Python list
- * @p: elemets of a python list
+ * print_python_list_info - prints info about a Python list object
+ * @p: pointer to PyObject representing a Python list
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size = PyList_Size(p);
-	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+	Py_ssize_t i, size;
+
+	size = PyList_Size(p);
 
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", allocated);
+
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
 }
