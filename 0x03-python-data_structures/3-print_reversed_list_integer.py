32f36c4afb035eb52224fafe7b29c563a4c9d643
theotsori <theodoretsori@gmail.com>
2022-12-05 15:10:06 +0300

    reverse list using str.format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    r_list = []
-    for i in my_list:
-        r_list.insert(0, i)
-    for x in range(len(r_list)):
-        print(r_list[x], sep="\n")
+    for i in range(len(my_list)-1, -1, -1):
+        print("{}".format(my_list[i]))
