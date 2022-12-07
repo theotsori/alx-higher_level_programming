ef4768f34eb6ba7073a919c21c76730dc41d3d16
theotsori <theodoretsori@gmail.com>
2022-12-07 23:14:00 +0300

    map lambda did well

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from string import roman
+def roman_to_int(roman_string):
+    for roman_string in range(1, 4000):
+        if roman_string != str or roman_string == None:
+            return 0
+        return int(roman_string)
