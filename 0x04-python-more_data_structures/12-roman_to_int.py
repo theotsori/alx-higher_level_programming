1099396fcd858a3bd3c068b9d3e04ada78dba886
theotsori <theodoretsori@gmail.com>
2022-12-08 08:34:00 +0300

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-from string import roman
 def roman_to_int(roman_string):
-    for roman_string in range(1, 4000):
-        if roman_string != str or roman_string == None:
-            return 0
-        return int(roman_string)
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+
+    roman_to_int_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    integer = 0
+    previous_value = 0
+    for ch in roman_string[::-1]:
+        current_value = roman_to_int_map[ch]
+        if current_value < previous_value:
+            integer -= current_value
+        else:
+            integer += current_value
+        previous_value = current_value
+
+    return integer
