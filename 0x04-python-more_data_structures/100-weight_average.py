7c50dc00e87364079d64838f313b1a48a62a4fda
theotsori <theodoretsori@gmail.com>
2022-12-10 00:15:53 +0300

    tuples average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,5 +2,10 @@
 def weight_average(my_list=[]):
     if not my_list:
         return (0)
-    rel = list(lambda x, y: (x*y)/y
-    print(rel)
+    score_sum = 0
+    weight_sum = 0
+    for score, weight in my_list:
+        score_sum += score * weight
+        weight_sum += weight
+
+    return score_sum / weight_sum
