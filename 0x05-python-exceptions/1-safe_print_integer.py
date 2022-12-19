27297e24eec0354763978aceef01ac1e9d1f03cc
theotsori <theodoretsori@gmail.com>
2022-12-19 14:14:44 +0300

    int except update 0x3

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)), end="\n")
         return True
-    except (ValueError, TypeError):
+    except (ValueError, NameError):
         return False
