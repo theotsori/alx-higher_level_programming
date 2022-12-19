c250a7da38ff0f2c2af4344ade9a609a282b7fbd
theotsori <theodoretsori@gmail.com>
2022-12-19 14:51:08 +0300

    print wit error msg 0x3

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
-        print("Exception: ", sys.exc_info()[1], file=sys.stderr)
+    except Exception as e:
+        print("Exception: ", e, file=sys.stderr)
         return False
