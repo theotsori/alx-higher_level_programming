a751224cf3ef3f11e3017138f70ffb0bb150eae2
theotsori <theodoretsori@gmail.com>
2022-12-21 08:36:29 +0300

    sie validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""A class representing a square"""
+
+
 class Square:
     def __init__(self, size=0):
+        """__init__ defined.
+
+        Args:
+            param (float): size is set to 0.
+
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
