798922de2cd3020b63ed1307ad3521984627e67c
theotsori <theodoretsori@gmail.com>
2022-12-21 11:34:26 +0300

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,15 +3,36 @@
 
 
 class Square:
+    """A class representing a square.
+
+    Attribute:
+        size (int): The size of the square.
+    """
+
     def __init__(self, size=0):
+    """Initailize a new square.
+
+    Args:
+        size (int, optional): The size of the square. Defaults to 0.
+    """
         self.size = size
 
     @property
     def size(self):
+    """int: The size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+    """Set the size of the square.
+
+    Args:
+        value (int): The new size of the square.
+
+    Raises:
+        TypeError: If 'value' is not an integer
+        ValuError: If 'value' is less tan 0.
+    """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -19,9 +40,11 @@ class Square:
         self.__size = value
 
     def area(self):
+    """Return the current square area."""
         return self.size ** 2
 
     def my_print(self):
+    """Print the sqaure with the character #"""
         if self.size == 0:
             print()
         else:
