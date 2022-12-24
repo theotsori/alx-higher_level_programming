7f78284b023add24cc4c9fcb7e4b3fc488004e31
theotsori <theodoretsori@gmail.com>
2022-12-24 22:07:53 +0300

    add int txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+
 def add_integer(a, b=98):
-    """Python script that adds two integers"""
+    """Python script that adds two integers
+
+    Parameters:
+    a (int or float): The first number to be added.
+    b (int or float): The second number to be added. default is 98.
+
+    Returns:
+    int: The sum of a and b, casted into int
+
+    Raises:
+    TypeError: if a or b are not int or floats"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
