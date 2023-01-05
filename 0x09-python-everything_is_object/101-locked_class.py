b8f0784fcde8685010c2ac46819a65cc9c2fb7ad
theotsori <theodoretsori@gmail.com>
2023-01-05 19:23:47 +0300

    low memory costx5

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""A class LockedClass with no class or object attribute"""
+
+
 class LockedClass:
+    """A class Locked with no class"""
     def __setattr__(self, name, value):
         if name != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
