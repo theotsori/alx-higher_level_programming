1acfd1b3082b7c50b063df73bb0b2a69c1cc35e2
theotsori <theodoretsori@gmail.com>
2023-01-09 12:32:22 +0300

    lookupx1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""A module used tocheck attributes and methods"""
+
+
 def lookup(obj):
+    """function that returns list of available
+       attributes and methods of an object
+    """
     return dir(obj)
