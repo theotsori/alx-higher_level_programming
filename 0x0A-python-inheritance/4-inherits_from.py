b90b15e5c7154d5fe922e7a275f8d979452e0a15
theotsori <theodoretsori@gmail.com>
2023-01-09 16:36:40 +0300

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Object function that returns if obj isinstance"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of the specified class or
+    a subclass of the specified class
+    """
+    return isinstance(obj, a_class)
