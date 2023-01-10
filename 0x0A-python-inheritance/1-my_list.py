33d2e8bee2657fc0baa372af904cd664b1e66d32
theotsori <theodoretsori@gmail.com>
2023-01-10 08:47:49 +0300

    my list x1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 
 
 class MyList(list):
-
+    """A class mylist that inherits from list"""
     def print_sorted(self):
         """ Create a copy of the list and sort it """
         sorted_list = self.copy()
