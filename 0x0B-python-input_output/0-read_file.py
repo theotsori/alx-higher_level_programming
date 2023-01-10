3dc244130e0197d8fa3622a5afa6b9cfcb960610
theotsori <theodoretsori@gmail.com>
2023-01-10 13:51:37 +0300

    Read filex1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """Function that reads a file"""
     with open('my_file_0.txt', encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read())
