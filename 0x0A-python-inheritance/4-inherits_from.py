1a4f940d902df96d2d894668e13f8686ebeec34d
theotsori <theodoretsori@gmail.com>
2023-01-10 09:05:17 +0300

    integer validator x2

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,5 @@ def inherits_from(obj, a_class):
     Check if the object is an instance of the specified class or
     a subclass of the specified class
     """
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
