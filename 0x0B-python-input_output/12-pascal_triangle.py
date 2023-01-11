85edd7863d615ebc7c9f367055e479586f9d9039
theotsori <theodoretsori@gmail.com>
2023-01-11 14:24:06 +0300

    Pascal's Triagle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that returns list of list rep pascal tri"""
+
+def pascal_triangle(n):
+    """Implementation of the pascal triangle function"""
+    if n <= 0:
+        return []
+    triangle = [[1] * (i + 1) for i in range(n)]
+    for i in range(n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    return triangle
