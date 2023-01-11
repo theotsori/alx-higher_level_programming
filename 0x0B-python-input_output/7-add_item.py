ecbd861e1a2579857db9acc46ddf6ecdde0e1416
theotsori <theodoretsori@gmail.com>
2023-01-11 10:07:06 +0300

    load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+   Py script that adds all args to py
+   list and save in a file
+"""
+import json
+import sys
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as json_file:
+        json_string = json.dumps(my_obj)
+        json_file.write(json_string)
+
+def load_from_json_file(filename):
+    with open(filename, 'r') as json_file:
+        json_string = json_file.read()
+        return json.loads(json_string)
+
+try:
+    data = load_from_json_file('add_item.json')
+except:
+    data = []
+
+for item in sys.argv[1:]:
+    data.append(item)
+
+save_to_json_file(data, 'add_item.json')
