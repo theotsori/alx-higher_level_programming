d1d556ac0c18950028d0c0432a4bae07dc24de1a
theotsori <theodoretsori@gmail.com>
2023-01-12 12:36:57 +0300

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,26 +3,37 @@
 A script that reads stdin line by line and computes metrics
 """
 import sys
-from collections import defaultdict
 
-status_codes = defaultdict(int)
-total_size = 0
-line_count = 0
+count = 0
+file_size = 0
+status_count = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+}
 
 try:
     for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
-        status_code = parts[-3]
-        file_size = int(parts[-1])
-        total_size += file_size
-        status_codes[status_code] += 1
+        count += 1
+        data = line.strip().split()
+        file_size += int(data[-1])
+        status = int(data[-2])
+        if status in status_count:
+            status_count[status] += 1
 
-        if line_count % 10 == 0:
-            print("Total file size: ", total_size)
-            for key in sorted(status_codes.keys()):
-                print("{}: {}".format(key, status_codes[key]))
+        if count % 10 == 0:
+            print("Total file size: ", file_size)
+            for status_code in sorted(status_count):
+                if status_count[status_code] > 0:
+                    print(f"{status_code}: {status_count[status_code]}")
+            print("\n")
 except KeyboardInterrupt:
-    print("Total file size: ", total_size)
-    for key in sorted(status_codes.keys()):
-        print("{}: {}".format(key, status_codes[key]))
+    print("Total file size: ", file_size)
+    for status_code in sorted(status_count):
+        if status_count[status_code] > 0:
+            print(f"{status_code}: {status_count[status_code]}")
