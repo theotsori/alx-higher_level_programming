ed18f6f4304055e14fe2c3955a618c6d48dc2b95
theotsori <theodoretsori@gmail.com>
2023-01-12 11:08:22 +0300

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -26,4 +26,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle] <{}>/<{}>".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
