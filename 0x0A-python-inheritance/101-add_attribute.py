d34a78795f94b6b4acdbba9aba80abcd3b5d0587
theotsori <theodoretsori@gmail.com>
2023-01-12 11:34:47 +0300

    Can I? x3

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,8 +4,6 @@
 
 def add_attribute(obj, name, value):
     """Implementation of the function"""
-    if not hasattr(obj, '__class__'):
-        raise TypeError("can't add new attribute")
-    if not hasattr(obj.__class__, '__setattr__'):
+    if not hasattr(obj, '__setattr__'):
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
