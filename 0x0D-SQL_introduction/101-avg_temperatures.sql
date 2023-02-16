1bd12d44496ee67c05196db4c1bc540dbc122684
theotsori <theodoretsori@gmail.com>
2023-02-16 14:50:53 +0300

    Temperatures #0

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- A script that displays the average temp by city ordered by temp
 
-SELECT city, AVG((temperature * 1.8) + 32) as avg_temp
+SELECT city, AVG(value) as avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
