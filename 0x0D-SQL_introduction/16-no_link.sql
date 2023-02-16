c637540713ad4397f80b617ef7bcd997cc4bd548
theotsori <theodoretsori@gmail.com>
2023-02-16 12:35:19 +0300

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- A script that lists all records of the second_table
+-- of the database hbtn_0c_0 in MySQL server
+
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
