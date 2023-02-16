f16f5696cb2b0a03b666e7f67ab35e32f0e1714e
theotsori <theodoretsori@gmail.com>
2023-02-16 11:44:29 +0300

    Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- A script that prints the descritpion of the first_table
 
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA='hbtn_0c_0' AND TABLE_NAME='first_table';
+SHOW COLUMNS FROM hbtn_0c_0.first_table;
