8acba25a09808b14376584d2fc4c3da209c21f23
theotsori <theodoretsori@gmail.com>
2023-02-17 18:38:43 +0300

    States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Script that creates a db hbtn_0d_usa and tables states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
