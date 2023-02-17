26e480b5b6a7947d044a395cf19453d890d5c693
theotsori <theodoretsori@gmail.com>
2023-02-17 18:19:14 +0300

    Always a name

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 -- A script that lists privileges of all my users
 
-SELECT User, Host, Grant_priv, Create_priv, Alter_priv, Drop_priv, Select_priv, Insert_priv, Update_priv, Delete_priv
-FROM mysql.user
-WHERE User IN ('user_0d_1', 'user_0d_2')
-AND Host = 'localhost';
+SELECT grantee, privilege_type
+FROM information_schema.user_privileges
+WHERE grantee IN ('user_0d_1@localhost', 'user_0d_2@localhost');
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- sql script that creates the db hbtn_0d_2 and the user_0d_2
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
