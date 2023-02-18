4e6e9ce97d351e8c25800ed9b1942e893bd277b8
theotsori <theodoretsori@gmail.com>
2023-02-18 10:24:00 +0300

    Cities by states

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,9 +4,6 @@ USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-) AND states.name = 'California'
+WHERE cities.state_id = states.id 
+AND states.name = 'California'
 ORDER BY cities.id ASC;
---
+++ 0x0E-SQL_more_queries/9-cities_by_states_join.sql
@@ -0,0 +1,6 @@
+-- sql Script that lists all cities contained in the database
+
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id ASC;
