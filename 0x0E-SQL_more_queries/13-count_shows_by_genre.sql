5b56217cb0cdcd2489a6da094da712026fb2d761
theotsori <theodoretsori@gmail.com>
2023-02-18 11:55:26 +0300

    Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- A script that lists all genres and displays no. of shows linked
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING COUNT(tv_show_genres.genre_id) > 0
+ORDER BY number_of_shows DESC;
