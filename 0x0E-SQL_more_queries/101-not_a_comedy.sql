9cfd5562481eda7fd2508920bd7b3c9def712b2c
theotsori <theodoretsori@gmail.com>
2023-02-18 12:52:43 +0300

    No comedy tonightx1

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,12 +3,12 @@
 SELECT title
 FROM tv_shows
 WHERE id NOT IN (
-  SELECT show_id
-  FROM tv_show_genres
-  WHERE genre_id = (
-    SELECT id
-    FROM tv_genres
-    WHERE name = 'Comedy'
-  )
+    SELECT show_id
+    FROM tv_show_genres
+    WHERE genre_id = (
+        SELECT id
+        FROM tv_genres
+        WHERE name = 'Comedy'
+    )
 )
 ORDER BY title ASC;
