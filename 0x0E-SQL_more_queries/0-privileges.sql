d874df62cf834868cab804d955c8d028aefe79f4
theotsori <theodoretsori@gmail.com>
2023-02-20 08:37:53 +0300

    My privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,9 @@
 -- A script that lists privileges of all my users
 
-SELECT * FROM mysql.user WHERE User = 'user_0d_1' OR User = 'user_0d_2';
+SELECT
+    grantee,
+    privilege_type
+FROM
+    information_schema.user_privileges
+WHERE
+    grantee IN ('user_0d_1@localhost', 'user_0d_2@localhost');
