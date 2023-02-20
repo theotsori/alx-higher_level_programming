028f8ac2752028ae58d5c931339bab8d97e91984
theotsori <theodoretsori@gmail.com>
2023-02-20 09:01:04 +0300

    Best Genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,9 +1,11 @@
 -- Script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
 
-SELECT tv_genres.name, SUM(rating)
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN hbtn_0d_tvshows_rate ON hbtn_0d_tvshows_rate.tv_show_id = tv_shows.id
-GROUP BY tv_genres.id
-ORDER BY SUM(rating) DESC;
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
