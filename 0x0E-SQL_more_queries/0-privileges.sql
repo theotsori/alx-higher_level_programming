1119a35c8a11df278795604b9b3557e273d30964
theotsori <theodoretsori@gmail.com>
2023-02-20 09:04:28 +0300

    My privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,4 @@
 -- A script that lists privileges of all my users
 
-SELECT
-    grantee,
-    privilege_type
-FROM
-    information_schema.user_privileges
-WHERE
-    grantee IN ('user_0d_1@localhost', 'user_0d_2@localhost');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
