db220334308099cb3cc91fccf230dcdb153204ac
theotsori <theodoretsori@gmail.com>
2023-03-13 21:00:10 +0300

    Increment

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(let i = 0; i < myArray.length; ++i) {
-console.log(`${myArray[i]}`);
+for (let i = 0; i < myArray.length; ++i) {
+  console.log(`${myArray[i]}`);
 }
