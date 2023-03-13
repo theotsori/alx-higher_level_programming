4af9a9cb1996ab54f0dd6d53a157eecd02f1d2e2
theotsori <theodoretsori@gmail.com>
2023-03-13 20:27:31 +0300

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
 
-const x = parseInt(process.argv.length);
-let first = parseInt(process.argv[2]);
-let second = parseInt(proces)
-
-function secondBig(x) {
-
-  for(let i = 0; i <= x.length; i++){
-    if(x[i] > first){
-      second = first;
-      first = x[i];
-    }
-    else if (x[i] > second && x[i] != first) {
-      second = x[i];
-    }
-  }
-  console.log(second);
+const args = process.argv.slice(2).map(Number); // convert arguments to array of numbers
+if (args.length < 2) {
+  console.log(0); // print 0 if there are less than 2 arguments
+} else {
+  const max = Math.max(...args); // find the maximum value in the array
+  const secondMax = Math.max(...args.filter(num => num !== max)); // find the second maximum value in the array
+  console.log(secondMax); // print the second maximum value
 }
-
-secondBig(x);
