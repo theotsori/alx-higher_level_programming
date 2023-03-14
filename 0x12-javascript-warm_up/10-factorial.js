6d08635813c23f1c60fab878afeb42497948ca0f
theotsori <theodoretsori@gmail.com>
2023-03-14 11:43:03 +0300

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,9 +2,14 @@
 
 const x = parseInt(process.argv[2]);
 
-let fact = 1;
-
-for (let i = 1; i <= x; i++) {
-  fact *= i;
+function factorial(x) {
+  if (isNaN(x)) {
+    return 1;
+  } else if (x <= 1) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
 }
-console.log(`${fact}`);
+
+console.log(factorial(x));
