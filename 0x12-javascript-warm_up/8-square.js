921d89825bfd714b6cd24f2151d1fbc652226995
theotsori <theodoretsori@gmail.com>
2023-03-14 09:51:54 +0300

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 
 const x = parseInt(process.argv[2]);
-let string = '';
 
 if (!x) {
   console.log('Missing size');
-}
-
-for (let i = 0; i < x; ++i) {
-  for (let j = 0; j < x; ++j) {
-    string += 'x';
+} else {
+  for (let i = 0; i < x; i++) {
+    let string = "";
+    for (let j = 0; j < x; j++) {
+      string += 'x';
+    }
+    console.log(string);
   }
-  string += '\n';
 }
-console.log(string);
