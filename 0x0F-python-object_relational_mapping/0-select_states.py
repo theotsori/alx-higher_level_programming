5f363deef6d4362cac43b156f86985e7dffd658a
theotsori <theodoretsori@gmail.com>
2023-03-17 11:01:09 +0300

    Select state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-
+"""A script that lists all states from the database
+   hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    """ Get mysql username, password, and database from cmd
+    """
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
-                         user='root',
-                         passwd='Theowein@23',
-                         db='hbtn_0e_0_usa')
+                         user=username,
+                         passwd=password,
+                         db=database)
 
     cursor = db.cursor()
 
