51b8f576c9c4fefc2a18bc2700bb69bb280f1628
theotsori <theodoretsori@gmail.com>
2023-03-20 14:08:55 +0300

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,23 +5,19 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    """ Get command line arguments
-    """
+
     mysql_user = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    conn = MySQLdb.connect(host='localhost',
-                           port=3306,
-                           user=mysql_user,
-                           passwd=mysql_password,
-                           db=db_name)
+    conn = MySQLdb.connect(host='localhost', port=3306,
+      user=mysql_user, passwd=mysql_password, db=db_name)
 
     cursor = conn.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE \
-        '{}' ORDER BY id".format(state_name)
+    query = "SELECT * FROM states WHERE name \
+        LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
     cursor.execute(query)
 
     results = cursor.fetchall()
@@ -29,3 +25,4 @@ if __name__ == '__main__':
         print(row)
 
     cursor.close()
+    conn.close()
