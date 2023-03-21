4add9c20ae6e53c9628dffa0fb0f8f0138c7dde5
theotsori <theodoretsori@gmail.com>
2023-03-21 10:10:38 +0300

    list relationship from city

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# Adds the State "California" with the City "San Francisco"
-# to the database hbtn_0e_100_usa using SQLAlchemy and
-# the cities relationship for all State objects.
-
+"""
+Adds the State "California" with the City "San Francisco"
+to the database hbtn_0e_100_usa using SQLAlchemy and
+the cities relationship for all State objects.
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -11,27 +12,20 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == '__main__':
-    # Create an engine to the database
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.
         format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
 
-    # Create a Session instance
     session = Session()
 
-    # Create a new State and City objects
     new_state = State(name="California")
     new_city = City(name="San Francisco")
 
-    # Add the new City to the new State's cities relationship
     new_state.cities.append(new_city)
 
-    # Add the new State to the session and commit changes
     session.add(new_state)
     session.commit()
 
-    # Close the session
     session.close()
--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -11,23 +11,19 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    # Set up connection to the database
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(username, password, database))
 
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
 
-    # Create a Session instance
     session = Session()
 
-    # Query the database
     states = session.query(State).order_by(State.id).all()
 
-    # Print the results
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
