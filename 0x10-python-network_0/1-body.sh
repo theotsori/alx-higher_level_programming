de492ac8ef62794607f5612bdef9dedc85b1c46f
theotsori <theodoretsori@gmail.com>
2023-03-30 15:42:38 +0300

    cURL only methods

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL using curl, and store the response in a variable
-[[ $# -eq 0 ]] && [[ $(curl -s -w "%{http_code}" "$1") -eq 200 ]] && curl -s "$1" || curl -s -w "%{http_code}" "$1"
+[[ $(curl -s -w "%{http_code}" "$1") -eq 200 ]] && curl -s "$1" || curl -s -w "%{http_code}" "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to send an OPTIONS request and display the allowed methods
-curl -s -I -X OPTIONS "$1" | awk '/Allow/ {print $2}'
+curl -s -I -X OPTIONS "$1" | grep -i Allow
