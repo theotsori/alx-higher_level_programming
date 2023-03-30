5e20675c581239e320f9ee440d1c8c9e95491fa8
theotsori <theodoretsori@gmail.com>
2023-03-30 13:37:23 +0300

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that finds a peak in a list of unsorted ints
+"""
+def find_peak(list_of_integers):
+    """function that finds a peak in a list
+    """
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid+1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
