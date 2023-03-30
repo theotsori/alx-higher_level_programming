72b8d2401c4351edd95217fe17cf9d7b4babe380
theotsori <theodoretsori@gmail.com>
2023-03-30 02:40:41 -0700

    Content

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# Send request to the url and save the response
 
-url=$1
-response=$(curl -sI "$url" | grep -i content-length | awk '{print $2}')
-echo "$response"
+response=$(curl -sI $1)
+
+# Extract the Content-Length header from the response and remove whitespace
+content_length=$(echo "$response" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r')
+
+# Output the size of the response body in bytes
+echo "$content_length"
