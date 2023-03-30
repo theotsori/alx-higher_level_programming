2cdb84378e82c94d241b36e05c866ebcbf30e132
theotsori <theodoretsori@gmail.com>
2023-03-30 14:09:20 +0300

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# Send request to the url and save the response
-
-response=$(curl -sI $1)
-
-# Extract the Content-Length header from the response and remove whitespace
-content_length=$(echo "$response" | grep -i "Content-Length:" | awk '{print $2}' | tr -d '\r')
-
-# Output the size of the response body in bytes
-echo "$content_length"
+[[ $# -eq 0 ]] && response=$(curl -s -w "%{size_download}" -o /dev/null "$1"); echo "$response"
