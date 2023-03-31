62152b810f9d7f255c0f3d61d93fcb6dd2b8a6f4
theotsori <theodoretsori@gmail.com>
2023-03-31 14:57:24 +0300

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to the URL using curl, and store the response in a variable
-[[ $(curl -s -w "%{http_code}" "$1") -eq 200 ]] && curl -s "$1" || curl -s -w "%{http_code}" "$1"
+# Sends a GET request to a URL and displays the body of the response if the status code is 200
+curl -s "$1" -o /dev/null -w "%{http_code}\n" | grep -q 200 && curl -s "$1"
