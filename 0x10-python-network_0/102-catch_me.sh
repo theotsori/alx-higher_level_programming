c5c60bd831f84c5f746143dff04356ed8ad81d26
theotsori <theodoretsori@gmail.com>
2023-03-31 15:05:18 +0300

    cURL to the end

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a POST request to the specified URL with data for server to respond "You got me!"
-curl -s -X POST -d "user_id=98" -H "Referer: http://0.0.0.0:5000/catch_me" http://0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing "You got me!" in the body of the response
+curl -sLX PUT 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool" -d "user_id=98"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send the GET request and display the body of the response
-curl -H "X-School-User-Id: 98" "$1"
+curl -s -H "X-School-User-Id: 98" "$1"
