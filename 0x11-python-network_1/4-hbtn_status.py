c5c4fb75710f86460d6f71411bead5572ebe0d95
theotsori <theodoretsori@gmail.com>
2023-04-01 09:29:34 +0300

    Whats my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,5 +11,4 @@ if __name__ == "__main__":
 
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.content))
-    print("\t- utf8 content: {}".format(response.text))
+    print("\t- content: {}".format(response.text))
