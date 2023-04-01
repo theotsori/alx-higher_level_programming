df1e4850a4df36e84046f5f9fe7e2cc0d4acdbcb
theotsori <theodoretsori@gmail.com>
2023-04-01 09:40:19 +0300

    Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
-        print(response.tet)
+        print(response.text)
