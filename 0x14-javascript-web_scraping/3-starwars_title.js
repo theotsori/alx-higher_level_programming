c328ea0ac25f003cf817f534986b2fcb68208fc5
theotsori <theodoretsori@gmail.com>
2023-04-25 10:44:02 +0300

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+'use strcit';
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request.get(url + movieId, function (error, response, body) {
+    if (error) {
+        console.error(error);
+        return;
+    } else {
+        console.log(JSON.parse(body).title);
+    }
+});
