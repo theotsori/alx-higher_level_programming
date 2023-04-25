c3eaaf73ae51e08dd66b77e00b56c236e2299cdf
theotsori <theodoretsori@gmail.com>
2023-04-25 09:47:36 +0300

    Readme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+'use strict';
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+});
