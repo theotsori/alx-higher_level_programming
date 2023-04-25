d0b764d87585f69e327ed4ddf90055756d47ac29
theotsori <theodoretsori@gmail.com>
2023-04-25 11:36:28 +0300

    Who was laying in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+'use script';
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    const characters = JSON.parse(body).characters;
+
+    characters.forEach(characterUrl => {
+        request(characterUrl, function (error, response, body) {
+            if (error) {
+                console.error(error);
+                return;
+                }
+                const character = JSON.parse(body).name;
+                console.log(character);
+        });
+    });
+});
