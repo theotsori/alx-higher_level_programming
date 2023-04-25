4e53b088559db193b7c0b1f2e00127d9b77bfeef
theotsori <theodoretsori@gmail.com>
2023-04-25 11:49:55 +0300

    Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+'use script';
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.error(error);
+    } else {
+        const characters = JSON.parse(body).characters;
+        printCharacters(characters, 0);
+    }
+});
+
+function printCharacters(characters, index) {
+    if (index >= characters.length) {
+        return;
+    }
+    request(characters[index], function (error, response, body) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(JSON.parse(body).name);
+            printCharacters(characters, index + 1);
+        }
+    });
+}
