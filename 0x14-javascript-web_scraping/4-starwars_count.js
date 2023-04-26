d224b95644bd5dbbbd4f8c447c634172e6198cce
theotsori <theodoretsori@gmail.com>
2023-04-26 09:51:44 +0300

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,15 @@
 const request = require('request');
 const apiUrl = process.argv[2];
 
-request(apiUrl, (error, response, body) => {
+request(apiUrl, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const film of films) {
-      const characters = film.characters;
-      if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count++;
-      }
-    }
-    console.log(count);
+    return;
   }
+
+  const moviesWithWedge = body.results.filter(movie =>
+    movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+  );
+
+  console.log(moviesWithWedge.length);
 });
