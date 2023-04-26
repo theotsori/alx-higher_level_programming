ebd922b3cb12d6b3edb1985306f20616fd00c6d5
theotsori <theodoretsori@gmail.com>
2023-04-26 08:47:52 +0300

    Readme v1.1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
