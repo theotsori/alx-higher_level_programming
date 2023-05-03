ccb30c2e0c717be164b1f3d73cb262347a893552
theotsori <theodoretsori@gmail.com>
2023-05-03 13:14:34 +0300

    And press Enter

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,18 @@
+// script that fetches and prints how to say “Hello” depending on the language
+$(document).ready(function () {
+  $('#btn_translate').click(function () {
+    const langCode = $('#language_code').val();
+    const url = `https://www.fourtonfish.com/hellosalut/?lang=${langCode}`;
+
+    $.get(url, function (data) {
+      $('#hello').text(data.hello);
+    });
+  });
+
+  $('#language_code').keypress(function (event) {
+    const keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      $('#btn_translate').click();
+    }
+  });
+});
