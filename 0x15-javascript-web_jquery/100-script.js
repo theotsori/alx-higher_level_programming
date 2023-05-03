57b86a5746887e5a0e3b4c9976e7a4c1d6910c8b
theotsori <theodoretsori@gmail.com>
2023-05-03 12:54:16 +0300

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// script that updates the text color of the header
+document.addEventListener("DOMContentLoaded", function() {
+  var header = document.querySelector("header");
+  header.style.color = "#FF0000";
+});
