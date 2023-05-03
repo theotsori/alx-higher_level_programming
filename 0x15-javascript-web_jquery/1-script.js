b8bf6d639504050101aa729d269bd380613ddb07
theotsori <theodoretsori@gmail.com>
2023-05-03 10:09:26 +0300

    Click and turn red

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,4 +1,4 @@
 // Script that updates color of the <header> to #ff0000
-$(document).ready(function() {
+$(document).ready(function () {
   $('header').css('color', '#FF0000');
 });
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// jquery function that turns header to red when clicked
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
+});
